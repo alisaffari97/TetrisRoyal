2c520aba94c09c1825c9bc48b08ce03fe28442b5
Alexandre Alves <pynjunior@gmail.com>
2019-12-31 16:09:59 +0000

    Adding the rest of the files

---
+++ Assets/Scripts/GameSetupController.cs
@@ -0,0 +1,23 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+public class GameSetupController : MonoBehaviour
+{
+    // This script will be added to any multiplayer scene
+    void Start()
+    {
+        CreatePlayer(); //Create a networked player object for each player that loads into the multiplayer scenes.
+    }
+    private void CreatePlayer()
+    {
+        Debug.Log("Creating Player");
+
+    }
+
+    private void FixedUpdate()
+    {
+                 
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private PhotonView PV;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PV = GetComponent<PhotonView>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (PV.IsMine)
+        {
+            BasicMovement();
+        }
+    }
+
+    void BasicMovement()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            transform.position += new Vector3(-1, 0, 0);
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            transform.position += new Vector3(1, 0, 0);
+        }
+        else if (Input.GetKeyDown(KeyCode.W))
+        {
+            transform.position += new Vector3(0, 1, 0);
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            transform.position += new Vector3(0, -1, 0);
+        }
+    }
+
+}
